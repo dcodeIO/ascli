f3e8a227213d41baeecaf21e32ef871c5c5cfbea
Daniel Wirtz <daniel@code-emitter.com>
2013-05-28 20:23:40 +0200

    Numerals

--- tests/test.js
+++ tests/test.js
@@ -5,4 +5,5 @@ console.log("Hello world!".white.bold);
 console.log("...of ascli\n");
 console.log("Command line arguments".white.bold);
 console.log(ascli.opt, ascli.argv);
+ascli.banner("abcdefghijklmnopqrstuvwxyz 0123456789");
 ascli.ok("yep, that worked.");
